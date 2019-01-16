f7d6e8216d4860add243edb3ecea22cad237bde2
michal1324@hotmail.com <michal1324@hotmail.com>
2019-01-16 23:32:53 +0100

    remove files

--- src/components/NotFoundPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotFoundPage = () => (
-  <div>
-      The page that you are looking for does&apos;not exsist
-  </div>
-);
-
-export default NotFoundPage;
--- src/stores/index.js
+++
@@ -1,9 +0,0 @@
-import ModalStore from './ModalStore';
-import BeerListStore from './BeerListStore';
-
-const stores = {
-  ModalStore,
-  BeerListStore,
-};
-
-export default stores;
